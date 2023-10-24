46f5cb39307cc5407a38b56b4e2d8c0de7b88692
Pranshu <161811419821x@gmail.com>
2023-10-25 01:01:59 +0530

    almost done

--- HousePricePredictor/main.py
+++ HousePricePredictor/main.py
@@ -1,12 +1,20 @@
+import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+data = pd.read_csv("E:\Projects\MLway\HousePricePredictor\Final_data.csv")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    locations = sorted(data["location"].unique())
+    return render_template("index.html", locations=locations)
+
+
+@app.route("/predict", methods=["Post"])
+def predict():
+    return ""
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5500)
